018050788fc4de1410049835543ad7a15a9a6812
Alexandr Furmanchuk <afurmanchuk@neoflex.ru>
2023-01-12 17:25:11 +0300

    fix

--- __brick__/lib/main.dart
+++ __brick__/lib/main.dart
@@ -7,7 +7,5 @@ void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await configureDependencies();
 
-  runApp({
-    {name.pascalCase()}
-  }(router: GetIt.I()));
+  runApp({{name.pascalCase()}}(router: GetIt.I()));
 }
