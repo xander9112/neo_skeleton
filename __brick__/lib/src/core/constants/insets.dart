2121381d98c930b550e7d49f317f6415cfc0559a
Alexandr Furmanchuk <afurmanchuk@neoflex.ru>
2023-01-12 17:31:08 +0300

    fix

--- __brick__/lib/src/core/constants/insets.dart
+++ __brick__/lib/src/core/constants/insets.dart
@@ -77,3 +77,18 @@ class InsetsExtension extends ThemeExtension<InsetsExtension> {
     );
   }
 }
+
+class BorderRadiusInsets {
+  static const double small = 8.0;
+  static const double medium = 16.0;
+  static const double large = 32.0;
+
+  static const double xxs = 2.0;
+  static const double xs = 4.0;
+  static const double s = 8.0;
+  static const double m = 12.0;
+  static const double l = 16.0;
+  static const double xl = 24.0;
+  static const double xxl = 32.0;
+  static const double xxxl = 64.0;
+}
