f8f051a7bc31785d13266b3341155c3c76cdb80d
Alexandr Furmanchuk <afurmanchuk@neoflex.ru>
2023-01-16 15:59:45 +0300

    fix

--- __brick__/ios/Runner/AppDelegate.swift
+++ __brick__/ios/Runner/AppDelegate.swift
@@ -7,7 +7,21 @@ import Flutter
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
-    GeneratedPluginRegistrant.register(with: self)
-    return super.application(application, didFinishLaunchingWithOptions: launchOptions)
+      GeneratedPluginRegistrant.register(with: self)
+            
+      let controller = window.rootViewController as! FlutterViewController
+
+      let flavorChannel = FlutterMethodChannel(
+        name: "flavor",
+        binaryMessenger: controller.binaryMessenger)
+
+      flavorChannel.setMethodCallHandler({(call: FlutterMethodCall, result: @escaping FlutterResult) -> Void in
+        // Note: this method is invoked on the UI thread
+        let flavor = Bundle.main.infoDictionary?["App - Flavor"]
+          
+        result(flavor)
+      })
+
+      return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
 }
