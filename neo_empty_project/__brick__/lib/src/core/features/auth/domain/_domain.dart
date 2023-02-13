2464904fa1dba80f4b140e330a8c9a98c2a564ac
Alexandr Furmanchuk <afurmanchuk@neoflex.ru>
2023-02-13 23:42:55 +0300

    fix

--- neo_empty_project/__brick__/lib/src/core/features/auth/domain/_domain.dart
+++ neo_empty_project/__brick__/lib/src/core/features/auth/domain/_domain.dart
@@ -1,6 +1,4 @@
 export 'manager/auth_manager.dart';
 export 'model/_model.dart';
 export 'repository/_repository.dart';
-export 'usecase/_usecase.dart';
-export 'usecase/set_biometry_setting_use_case.dart';
-export 'usecase/set_new_pin_code_use_case.dart';
+export 'use_cases/_use_cases.dart';
