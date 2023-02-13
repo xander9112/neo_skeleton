aff3dc3f0ad7d4be1ac70680d96f83303f78f1ad
Alexandr Furmanchuk <afurmanchuk@neoflex.ru>
2023-02-13 22:39:18 +0300

    fix

--- hooks/post_gen.dart
+++ hooks/post_gen.dart
@@ -4,13 +4,57 @@ import 'package:mason/mason.dart';
 Future<void> run(HookContext context) async {
   final progress = context.logger.progress('Installing packages');
 
+  await _installPackages(context);
+
+  await _buildProject(context);
+
+  progress.complete();
+}
+
+Future<void> _installPackages(HookContext context) async {
   context.logger.info('Start install packages');
 
+  bool done = false;
+
   try {
-    await Process.run('flutter', ['packages', 'get']);
-  } catch (_) {
-    await Process.run('fvm', ['flutter', 'packages', 'get']);
-  } finally {
-    progress.complete();
+    await Process.run(
+      'flutter',
+      ['packages', 'get'],
+    );
+
+    done = true;
+  } catch (_) {}
+
+  if (!done) {
+    try {
+      await Process.run(
+        'fvm',
+        ['flutter', "pub", "run", "build_runner", "build"],
+      );
+    } catch (_) {}
+  }
+}
+
+Future<void> _buildProject(HookContext context) async {
+  context.logger.info('Start build project');
+
+  bool done = false;
+
+  try {
+    await Process.run(
+      'flutter',
+      ["pub", "run", "build_runner", "build"],
+    );
+
+    done = true;
+  } catch (_) {}
+
+  if (!done) {
+    try {
+      await Process.run(
+        'fvm',
+        ['flutter', "pub", "run", "build_runner", "build"],
+      );
+    } catch (_) {}
   }
 }
