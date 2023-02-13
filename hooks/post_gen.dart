75efdec702d5744a9cb65b0d12142d55e7302fc6
Alexandr Furmanchuk <afurmanchuk@neoflex.ru>
2023-02-13 20:49:07 +0300

    fix

--- hooks/post_gen.dart
+++ hooks/post_gen.dart
@@ -5,7 +5,8 @@ Future<void> run(HookContext context) async {
   final progress = context.logger.progress('Installing packages');
 
   // Run `flutter packages get` after generation.
-  await Process.run('flutter', ['packages', 'get']);
+
+  await Process.run('dart', ['init_project.dart']);
 
   progress.complete();
 }
