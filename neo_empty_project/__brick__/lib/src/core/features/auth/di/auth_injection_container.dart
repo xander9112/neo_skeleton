4ee0f59a269d7df0f8cd047eeab569ce783e439e
Alexandr Furmanchuk <afurmanchuk@neoflex.ru>
2023-02-13 23:56:53 +0300

    fix

--- neo_empty_project/__brick__/lib/src/core/features/auth/di/auth_injection_container.dart
+++ neo_empty_project/__brick__/lib/src/core/features/auth/di/auth_injection_container.dart
@@ -20,7 +20,7 @@ class AuthInjection extends ICoreInjection {
           biometricRepository: sl(),
           settings: AuthManagerSettings(
             useBiometric: !kIsWeb,
-            useLocalAuth: {{#useLocalAuth}}{{/useLocalAuth}},
+            useLocalAuth: {{#useLocalAuth}}true{{/useLocalAuth}}{{^useLocalAuth}}false{{/useLocalAuth}},
           ),
         ),
       )
