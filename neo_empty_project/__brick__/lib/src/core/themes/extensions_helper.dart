a4e402313a4b93f905de74d1d0130abe20440d30
Alexandr Furmanchuk <afurmanchuk@neoflex.ru>
2023-02-14 10:23:31 +0300

    fix

--- neo_empty_project/__brick__/lib/src/core/themes/extensions_helper.dart
+++ neo_empty_project/__brick__/lib/src/core/themes/extensions_helper.dart
@@ -54,3 +54,39 @@ extension BuildContextExt on BuildContext {
 
   bool get isLinux => platform == TargetPlatform.linux;
 }
+
+extension ScaffoldExt on BuildContext {
+  ScaffoldFeatureController<SnackBar, SnackBarClosedReason> showSnackBar(
+    SnackBar snackbar,
+  ) =>
+      ScaffoldMessenger.of(this).showSnackBar(snackbar);
+
+  void removeCurrentSnackBar({
+    SnackBarClosedReason reason = SnackBarClosedReason.remove,
+  }) =>
+      ScaffoldMessenger.of(this).removeCurrentSnackBar(reason: reason);
+
+  void hideCurrentSnackBar({
+    SnackBarClosedReason reason = SnackBarClosedReason.hide,
+  }) =>
+      ScaffoldMessenger.of(this).hideCurrentSnackBar(reason: reason);
+
+  void openDrawer() => Scaffold.of(this).openDrawer();
+
+  void openEndDrawer() => Scaffold.of(this).openEndDrawer();
+
+  void showBottomSheet<T>(
+    WidgetBuilder builder, {
+    Color? backgroundColor,
+    double? elevation,
+    ShapeBorder? shape,
+    Clip? clipBehaviour,
+  }) =>
+      Scaffold.of(this).showBottomSheet<T>(
+        builder,
+        backgroundColor: backgroundColor,
+        elevation: elevation,
+        shape: shape,
+        clipBehavior: clipBehaviour,
+      );
+}
