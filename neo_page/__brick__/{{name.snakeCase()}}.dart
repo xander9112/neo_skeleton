d29216b38e090456b30ed9a81663a8253b396b2f
Alexandr Furmanchuk <afurmanchuk@neoflex.ru>
2023-02-15 18:52:48 +0300

    fix

---
+++ neo_page/__brick__/{{name.snakeCase()}}.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+
+{{#stateful}}
+class {{name.pascalCase()}}Page extends StatefulWidget {
+  const {{name.pascalCase()}}Page({super.key});
+
+  @override
+  State<{{name.pascalCase()}}Page> createState() => _{{name.pascalCase()}}PageState();
+}
+
+class _{{name.pascalCase()}}PageState extends State<{{name.pascalCase()}}Page> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(title: const Text('{{name.pascalCase()}} page')),
+        body: Center(child: Text(state.users?.title ?? 'Unknown')),
+    );
+  }
+}
+{{/stateful}}
+
+{{^stateful}}
+class {{name.pascalCase()}}Page extends StatelessWidget {
+  const {{name.pascalCase()}}Page({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(title: const Text('{{name.pascalCase()}} page')),
+        body: Center(child: Text(state.users?.title ?? 'Unknown')),
+    );
+  }
+}
+
+{{/stateful}}
