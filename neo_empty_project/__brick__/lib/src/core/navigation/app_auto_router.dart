96adfea50e2648d5c6b8af240aad55c873549437
Alexandr Furmanchuk <afurmanchuk@neoflex.ru>
2023-04-25 10:33:08 +0300

    fix

--- neo_empty_project/__brick__/lib/src/core/navigation/app_auto_router.dart
+++ neo_empty_project/__brick__/lib/src/core/navigation/app_auto_router.dart
@@ -1,11 +1,11 @@
 import 'package:auto_route/auto_route.dart';
 import 'package:auto_route/empty_router_widgets.dart';
+import 'package:flutter/material.dart';
 import 'package:{{name.snakeCase()}}/src/core/_core.dart';
 import 'package:{{name.snakeCase()}}/src/features/home/_home.dart';
 import 'package:{{name.snakeCase()}}/src/features/main/_main.dart';
 import 'package:{{name.snakeCase()}}/src/features/settings/_settings.dart';
-import 'package:{{name.snakeCase()}}/src/features/splash/splash.dart';
-import 'package:flutter/material.dart';
+import 'package:{{name.snakeCase()}}/src/features/splash/_splash.dart';
 
 part 'app_auto_router.gr.dart';
 
