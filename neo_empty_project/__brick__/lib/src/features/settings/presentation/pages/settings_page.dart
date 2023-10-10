fa4f98e238b9c42179eefd70d5074a89c6539db1
Alexandr Furmanchuk <afurmanchuk@neoflex.ru>
2023-10-10 13:05:39 +0300

    fix

--- neo_empty_project/__brick__/lib/src/features/settings/presentation/pages/settings_page.dart
+++ neo_empty_project/__brick__/lib/src/features/settings/presentation/pages/settings_page.dart
@@ -1,5 +1,6 @@
 import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:{{name.snakeCase()}}/src/core/_core.dart';
 
 @RoutePage()
