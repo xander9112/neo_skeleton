a9c1a7e7a85f62043a656b5dff9c5c0d5c5f38e0
Alexander Furmanchuk <afurmanchuk@neoflex.ru>
2024-01-12 12:56:39 +0300

    fix

--- neo_feature_go_router/__brick__/{{name.snakeCase()}}/data/data_sources/mock_{{name.snakeCase()}}_data_source.dart
+++ neo_feature_go_router/__brick__/{{name.snakeCase()}}/data/data_sources/mock_{{name.snakeCase()}}_data_source.dart
@@ -2,9 +2,9 @@ import '../../_{{name.snakeCase()}}.dart';
 
 class Mock{{name.pascalCase()}}DataSource implements Remote{{name.pascalCase()}}DataSource {
   @override
-  Future<{{name.pascalCase()}}DTO> get{{name.pascalCase()}}() async {
-    await Future<void>.delayed(const Duration(seconds: 2));
-    
-    return const {{name.pascalCase()}}DTO(title: 'New {{name.pascalCase()}}');
+  Future<{{name.pascalCase()}}DTO> get{{name.pascalCase()}}() {
+    return Future<{{name.pascalCase()}}DTO>.delayed(const Duration(seconds: 2), () {
+      return const {{name.pascalCase()}}DTO(title: 'New {{name.pascalCase()}}');
+    });    
   }
 }
