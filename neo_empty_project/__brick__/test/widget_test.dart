f58c6f03fd0d01daa1eedbd243b30e3a6d49c67d
Alexander Furmanchuk <afurmanchuk@neoflex.ru>
2024-01-18 08:59:06 +0300

    fix

--- neo_empty_project/__brick__/test/widget_test.dart
+++ neo_empty_project/__brick__/test/widget_test.dart
@@ -6,11 +6,10 @@
 // tree, read text, and verify that the values of widget properties are correct.
 
 import 'package:flutter_test/flutter_test.dart';
-import 'package:get_it/get_it.dart';
-import 'package:{{name.snakeCase()}}/src/app.dart';
+import 'package:{{name.snakeCase()}}/src/core/{{name.snakeCase()}}_app.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
-    await tester.pumpWidget({{name.pascalCase()}}App(router: GetIt.I()));
+    await tester.pumpWidget(const {{name.pascalCase()}}App());
   });
 }
