d4b9d40e767e94b25f254c513ab63509fc3eae85
Alexander Furmanchuk <afurmanchuk@neoflex.ru>
2024-02-10 23:56:40 +0300

    fix

--- neo_feature/__brick__/{{name}}/data/data_sources/_data_sources.dart
+++ neo_feature/__brick__/{{name}}/data/data_sources/_data_sources.dart
@@ -1,2 +1,3 @@
 export 'mock_{{name.snakeCase()}}_data_source.dart';
 export 'rest_{{name.snakeCase()}}_data_source.dart';
+export '{{name.snakeCase()}}_data_source.dart';
--- neo_feature/__brick__/{{name}}/domain/data_sources/_data_sources.dart
+++
@@ -1,1 +0,0 @@
-export '{{name.snakeCase()}}_data_source.dart';
