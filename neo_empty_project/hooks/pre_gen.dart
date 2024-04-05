d28e73aa949a92d767d21f4643bdb577a75b5f82
Alexander Furmanchuk <afurmanchuk@neoflex.ru>
2024-04-05 13:19:56 +0300

    fix

--- neo_empty_project/hooks/pre_gen.dart
+++ neo_empty_project/hooks/pre_gen.dart
@@ -2,5 +2,24 @@ import 'package:mason/mason.dart';
 
 void run(HookContext context) {
   context.logger.info(context.vars.toString());
-  
+
+  context.vars = {
+    ...context.vars,
+    'androidBundleId':
+        '${context.vars['org']}.${_toCamelCase(context.vars['name'])}',
+    'iosBundleId':
+        '${context.vars['org']}.${_toCamelCase(context.vars['name'])}',
+  };
+}
+
+String _toCamelCase(String text) {
+  if (text.isEmpty) {
+    return '';
+  }
+
+  final words = text.split(' ');
+  final firstWord = words.first.toLowerCase();
+  final otherWords =
+      words.skip(1).map((word) => word[0].toUpperCase() + word.substring(1));
+  return firstWord + otherWords.join('');
 }
