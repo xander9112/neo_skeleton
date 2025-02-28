ff7bd146525259e03d2c8746198d7d0d9b7c2f8d
Merzlyakov Aleksey <amerzliakov@neoflex.ru>
2025-02-28 21:13:28 +0500

    fix

--- neo_feature/__brick__/{{name.snakeCase()}}/domain/repositories/{{name.snakeCase()}}_repository.dart
+++ neo_feature/__brick__/{{name.snakeCase()}}/domain/repositories/{{name.snakeCase()}}_repository.dart
@@ -1,6 +1,7 @@
 import 'package:core/core.dart';
+import 'package:dependencies/dependencies.dart';
 import 'package:{{projectName.snakeCase()}}/src/core/_core.dart';
-import '../../_{{name.snakeCase()}}.dart';
+import 'package:{{projectName.snakeCase()}}/src/features/{{name.snakeCase()}}/_{{name.snakeCase()}}.dart';
 
 // ignore: one_member_abstracts
 abstract class {{name.pascalCase()}}Repository {
